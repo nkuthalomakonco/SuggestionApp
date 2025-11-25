c40ec6ad754de110676b3b1d7bcfc981e6d5eb5b
nkuthalomakonco <nkuthalomakonco@gmail.com>
2025-11-25 15:08:25 +0200

    Models added

--- SuggestionAppUI/Program.cs
+++ SuggestionAppUI/Program.cs
@@ -1,10 +1,9 @@
+using SuggestionAppUI;
 using SuggestionAppUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
---
+++ SuggestionAppUI/RegisterServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+namespace SuggestionAppUI
+{
+    public static class RegisterServices
+    {
+
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            //builder.Services.AddRazorPages();
+            //builder.Services.AddServerSideBlazor(); 
+            builder.Services.AddMemoryCache();
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+        }
+    }
+}
