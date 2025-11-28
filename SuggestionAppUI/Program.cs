a8d421fee3e6aa746fd3ecf2ae749d722b4256c2
nkuthalomakonco <nkuthalomakonco@gmail.com>
2025-11-28 08:32:12 +0200

    Testing the sample page

--- SuggestionAppUI/Program.cs
+++ SuggestionAppUI/Program.cs
@@ -20,7 +20,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
