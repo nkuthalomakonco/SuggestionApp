2569d2b2bd53a610de24673ef49ad73c751b768e
nkuthalomakonco <nkuthalomakonco@gmail.com>
2025-11-28 10:55:04 +0200

    Generation sample data method.

--- SuggestionAppUI/GlobalUsings.cs
+++ SuggestionAppUI/GlobalUsings.cs
@@ -1,1 +1,2 @@
-﻿
+﻿global using SuggestionAppLibrary.Models;
+global using SuggestionAppLibrary.DataAccess;
--- SuggestionAppUI/Program.cs
+++ SuggestionAppUI/Program.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using SuggestionAppUI.Data;
+using SuggestionAppUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
